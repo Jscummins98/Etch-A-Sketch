e6d7816ae87cbc43f4334c4934bbfed68c4f0648
Jackson Cummins <jscummins98@gmail.com>
2022-11-07 19:44:24 -0500

    add functionality to the random color button

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ const sizeButton = document.querySelector("#size");
 sizeButton.addEventListener("click", createGrid);
 const container = document.querySelector('.container');
 
+// ***************Below code will give option buttons styling when they are clicked************/
+const optionButtons = document.querySelectorAll('.option');
+// function unClick(){
+//     optionButtons.forEach((button) =>{
+//         button.classList.remove('clickedButton');
+//     });
+// }
+optionButtons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        // unClick;
+        console.log('you clicked a button');
+        if(button.classList.contains("classic")){
+            console.log('classic');
+            optionButtons[0].classList.add('clickedButton');
+        }else if(button.classList.contains("random")){
+            console.log('random');
+            optionButtons[1].classList.add('clickedButton');
+        }else if(button.classList.contains("shade")){
+            console.log('shade');
+            optionButtons[2].classList.add('clickedButton');
+        }
+    });
+});
+
+//************Below code creates the grid  */
 function createGrid(){
     while(container.firstChild) container.removeChild(container.firstChild); //Clears the grid
 
@@ -28,4 +53,26 @@ function createGrid(){
 function getDimension(n) {
     let width = 1/n * 100;
     return width;
+}
+
+//********************Below code gives the color button functionality */
+const randomColorButton = document.querySelector('#random');
+randomColorButton.addEventListener("click", randomColorGrid);
+
+function randomColorGrid(){
+    let pixelNodeList = container.children;
+    for(pixel of pixelNodeList){
+        pixel.addEventListener('mouseenter', function(e){
+            e.target.style.backgroundColor = getRandomColor();
+        });
+    }
+}
+
+function getRandomColor(){
+    let hexArray = '0123456789ABCDEF';
+    let randomColor = '#';
+    for(let i = 0; i < 6; i++){
+        randomColor += hexArray[Math.floor(Math.random() * 16)];
+    }
+    return randomColor;
 }
